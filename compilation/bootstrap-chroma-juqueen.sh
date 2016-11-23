bfcdd255d91aec737c1240d0ac20180e14e0021b
Martin Ueding <dev@martin-ueding.de>
2016-11-23 20:07:52 +0100

    Extra clause for configure

--- compilation/bootstrap-chroma-juqueen.sh
+++ compilation/bootstrap-chroma-juqueen.sh
@@ -147,11 +147,13 @@ clone-if-needed git://git.gnome.org/libxml2 libxml2 v2.9.4
 pushd libxml2
 cflags="$base_cflags"
 cxxflags="$base_cxxflags"
-if ! [[ -f Makefile ]]; then
+if ! [[ -f configure ]]; then
     pushd m4
     ln -fs /usr/share/aclocal/pkg.m4 .
     popd
     NOCONFIGURE=yes ./autogen.sh
+fi
+if ! [[ -f Makefile ]]; then
     ./configure $base_configure \
         --without-zlib \
         --without-python \
