aa756a3cdd1d18f21a08233c7488d51a554fcf22
Martin Ueding <dev@martin-ueding.de>
2016-11-29 12:48:54 +0100

    Add chroma, use SSE2

--- compilation/jureca/chroma.sh
+++ compilation/jureca/chroma.sh
@@ -1,23 +1,27 @@
-print-fancy-heading qdpxx
+print-fancy-heading chroma
 
-clone-if-needed https://github.com/martin-ueding/qdpxx.git qdpxx martins-fedora24-build
+clone-if-needed https://github.com/martin-ueding/chroma.git chroma submodules-via-https
 
-pushd qdpxx
+set +x
+module load GMP
+set -x
+
+pushd chroma
 cflags="$base_cflags $openmp_flags"
-cxxflags="$base_cxxflags $openmp_flags $cxx11_flags"
+cxxflags="$base_cxxflags $openmp_flags"
 autoreconf-if-needed
 if ! [[ -f Makefile ]]; then
     ./configure $base_configure \
         --enable-openmp \
-        --enable-sse --enable-sse2 \
         --enable-parallel-arch=parscalar \
         --enable-parallel-io \
         --enable-precision=double \
         --enable-qdp-alignment=128 \
+        --enable-sse2 \
+        --with-gmp="$EBROOTGMP" \
         --with-libxml2="$prefix/bin/xml2-config" \
-        --with-qmp="$prefix" \
+        --with-qdp="$prefix" \
         CFLAGS="$cflags" CXXFLAGS="$cxxflags"
 fi
 make-make-install
 popd
-
