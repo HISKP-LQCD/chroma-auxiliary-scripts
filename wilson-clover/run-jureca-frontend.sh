eff1cd1e99fcd7f22e0f81149f68ebda1716d3f4
Martin Ueding <dev@martin-ueding.de>
2016-12-08 22:39:59 +0100

    Add frontend testing script

---
+++ wilson-clover/run-jureca-frontend.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+module load Intel
+module load IntelMPI
+
+OMP_NUM_THREADS=24 KMP_AFFINITY=scatter,0 \
+    ~/jureca-local-icc/bin/hmc -i wilson-clover.ini.xml -o wilson-clover.out.xml \
+    -by 8 -bz 8 -c 24 -sy 1 -sz 1 -pxy 1 -pxyz 0 -minct 2
