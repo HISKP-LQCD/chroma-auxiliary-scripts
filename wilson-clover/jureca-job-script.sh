5f75dd9ff431b3f74ee3fa501cca0919d4e5e081
Martin Ueding <dev@martin-ueding.de>
2016-12-08 20:02:52 +0100

    Add thread affinity and batch partition

--- wilson-clover/jureca-job-script.sh
+++ wilson-clover/jureca-job-script.sh
@@ -3,10 +3,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=00:30:00
-#SBATCH --partition=batch
+#SBATCH --partition=devel
 #SBATCH --cpus-per-task=24
 #SBATCH --mail-type=ALL
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-srun ./hmc -i testrun.ini.xml -o testrun.out.xml \
-    -by 8 -bz 8 -c 24 -sy 1 -sz 1 -pxy 1 -pxyz 0 -minct 2
+export KMP_AFFINITY=scatter,0
+srun ./hmc -i testrun.ini.xml -o testrun.out.xml -by 8 -bz 8 -c 24 -sy 1 -sz 1 -pxy 1 -pxyz 0 -minct 2
