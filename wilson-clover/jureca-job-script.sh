ae4cec0989123b58a64af33b601e58082d7b0851
Martin Ueding <dev@martin-ueding.de>
2016-12-14 21:24:37 +0100

    Attempt to use latest restart file

--- wilson-clover/jureca-job-script.sh
+++ wilson-clover/jureca-job-script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+# Copyright © 2016 Martin Ueding <dev@martin-ueding.de>
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -8,8 +9,17 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ueding@hiskp.uni-bonn.de
 
-last_restart=$(ls -rt *restart*.xml | tail -n 1)
+# Get the restart XML file that has been changed last in this directory.
+last_restart=$(ls -rt | grep restart | grep xml | tail -n 1)
 
+# In case no restart file can be found, use the start file. The `-` means
+# “default”, the `:` means that an empty but defined variable still counts as
+# undefined. That is exactly what we want here.
+input=${last_restart:-hmc-wilson-clover.ini.xml}
+
+# Set the approprite OpenMP variables for thread count and binding.
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 export KMP_AFFINITY=scatter,0
-srun ./hmc -i hmc-wilson-clover.ini.xml -o hmc-wilson-clover.out.xml -by 8 -bz 8 -c 24 -sy 1 -sz 1 -pxy 1 -pxyz 0 -minct 2
+
+# Start the Hybrid Monte Carlo simulation.
+srun ./hmc -i $input -o ${SLURM_JOB_ID}.out.xml -by 8 -bz 8 -c 24 -sy 1 -sz 1 -pxy 1 -pxyz 0 -minct 2
