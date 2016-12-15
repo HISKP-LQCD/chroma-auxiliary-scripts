e5d1ca4fa0bf4307fc45ed18a7996c1e0276d24f
Martin Ueding <dev@martin-ueding.de>
2016-12-15 13:14:30 +0100

    Move stuff around

--- wilson-clover/jureca/wilson-clover.slurm.sh
+++ wilson-clover/jureca/wilson-clover.slurm.sh
@@ -8,6 +8,9 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ueding@hiskp.uni-bonn.de
 
+module load Intel
+module load IntelMPI
+
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 export KMP_AFFINITY=scatter,0
 srun ./hmc -i hmc-wilson-clover.ini.xml -o hmc-wilson-clover.out.xml -by 8 -bz 8 -c 24 -sy 1 -sz 1 -pxy 1 -pxyz 0 -minct 2
