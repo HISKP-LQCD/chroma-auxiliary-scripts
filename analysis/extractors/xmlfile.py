57dd125053cc6633155eb3a92f6ffaa095b05c94
Martin Ueding <dev@martin-ueding.de>
2017-01-01 22:59:19 +0100

    Refactor the code

--- analysis/extractors/xmlfile.py
+++ analysis/extractors/xmlfile.py
@@ -11,22 +11,26 @@ from lxml import etree
 import numpy as np
 
 import extractors
-
+import transforms
 
 def main(options):
     for dirname in options.dirname:
-        extractors.print_progress(dirname)
-        for key, extractor in bits.items():
-            extracted = extractor(dirname)
-            if extracted is not None:
-                update_no_list, number_list = extracted
-                outfile = os.path.join(dirname, 'extract-{}.tsv'.format(key))
-                np.savetxt(outfile, np.column_stack([update_no_list, number_list]))
+        process_directory(dirname)
 
-        convert_to_md_time(dirname, 'w_plaq')
-        convert_to_md_time(dirname, 'deltaH')
-        convert_to_md_time(dirname, 'n_steps')
-        convert_time_to_minutes(dirname)
+
+def process_directory(dirname):
+    extractors.print_progress(dirname)
+    for key, extractor in bits.items():
+        extracted = extractor(dirname)
+        if extracted is not None:
+            update_no_list, number_list = extracted
+            outfile = os.path.join(dirname, 'extract-{}.tsv'.format(key))
+            np.savetxt(outfile, np.column_stack([update_no_list, number_list]))
+
+    transforms.convert_to_md_time(dirname, 'w_plaq')
+    transforms.convert_to_md_time(dirname, 'deltaH')
+    transforms.convert_to_md_time(dirname, 'n_steps')
+    transforms.convert_time_to_minutes(dirname)
 
 
 def make_xpath_extractor(xpath):
@@ -36,82 +40,46 @@ def make_xpath_extractor(xpath):
     return extractor
 
 
-def extract_n_steps(dirname):
-    xml_files = glob.glob(os.path.join(dirname, '*.xml'))
+def make_single_xpath_extractor(xpath):
+    def extractor(dirname):
+        xml_files = glob.glob(os.path.join(dirname, '*.xml'))
 
-    update_no_list = []
-    number_list = []
+        update_no_list = []
+        number_list = []
 
-    step_sizes = {}
+        step_sizes = {}
 
-    for xml_file in xml_files:
-        try:
-            tree = etree.parse(xml_file)
-        except etree.XMLSyntaxError as e:
-            print('XML file could not be loaded')
-            print(e)
-            continue
+        for xml_file in xml_files:
+            try:
+                tree = etree.parse(xml_file)
+            except etree.XMLSyntaxError as e:
+                print('XML file could not be loaded')
+                print(e)
+                continue
 
-        if len(tree.xpath('//doHMC')) == 0:
-            # This is not an output XML file.
-            continue
+            if len(tree.xpath('//doHMC')) == 0:
+                # This is not an output XML file.
+                continue
 
-        number = float(tree.xpath('//hmc/Input/Params/HMCTrj/MDIntegrator/Integrator/n_steps/text()')[0])
+            number = float(tree.xpath(xpath)[0])
 
-        updates = tree.xpath('//Update')
+            updates = tree.xpath('//Update')
 
-        for update in updates:
-            update_no = int(update.xpath('./update_no/text()')[0])
+            for update in updates:
+                update_no = int(update.xpath('./update_no/text()')[0])
 
-            if update_no in update_no_list:
-                # This number is already extracted for that update. Make sure
-                # that the extracted value is the same.
-                index = update_no_list.index(update_no)
-                assert number_list[index] == number
-            else:
-                update_no_list.append(update_no)
-                number_list.append(number)
+                if update_no in update_no_list:
+                    # This number is already extracted for that update. Make
+                    # sure that the extracted value is the same.
+                    index = update_no_list.index(update_no)
+                    assert number_list[index] == number
+                else:
+                    update_no_list.append(update_no)
+                    number_list.append(number)
 
-    return zip(*sorted(zip(update_no_list, number_list)))
+        return zip(*sorted(zip(update_no_list, number_list)))
 
-
-def extract_md_time(dirname):
-    xml_files = glob.glob(os.path.join(dirname, '*.xml'))
-
-    update_no_list = []
-    md_time_list = []
-
-    step_sizes = {}
-
-    for xml_file in xml_files:
-        try:
-            tree = etree.parse(xml_file)
-        except etree.XMLSyntaxError as e:
-            print('XML file could not be loaded')
-            print(e)
-            continue
-
-        if len(tree.xpath('//doHMC')) == 0:
-            # This is not an output XML file.
-            continue
-
-        tau0 = float(tree.xpath('//hmc/Input/Params/HMCTrj/MDIntegrator/tau0/text()')[0])
-
-        updates = tree.xpath('//Update')
-
-        for update in updates:
-            update_no = int(update.xpath('./update_no/text()')[0])
-
-            step_sizes[update_no] = tau0
-
-    md_time = 0.0
-
-    for update_no, tau0 in sorted(step_sizes.items()):
-        update_no_list.append(update_no)
-        md_time += tau0
-        md_time_list.append(md_time)
-
-    return update_no_list, md_time_list
+    return extractor
 
 
 def extract_xpath_from_all(xml_files, xpath):
@@ -161,42 +129,6 @@ bits = {
     'w_plaq': make_xpath_extractor('.//w_plaq/text()'),
     'deltaH': make_xpath_extractor('.//deltaH/text()'),
     'seconds_for_trajectory': make_xpath_extractor('.//seconds_for_trajectory/text()'),
-    'md_time': extract_md_time,
-    'n_steps': extract_n_steps,
+    'md_time': make_single_xpath_extractor('//hmc/Input/Params/HMCTrj/MDIntegrator/tau0/text()'),
+    'n_steps': make_single_xpath_extractor('//hmc/Input/Params/HMCTrj/MDIntegrator/Integrator/n_steps/text()'),
 }
-
-
-def convert_to_md_time(dirname, name_in):
-    file_in = os.path.join(dirname, 'extract-md_time.tsv')
-    data = np.atleast_2d(np.loadtxt(file_in))
-
-    if data.shape[1] == 0:
-        return
-
-    update_no = data[:, 0]
-    md_time = data[:, 1]
-
-    data = np.atleast_2d(np.loadtxt(os.path.join(dirname, 'extract-{}.tsv'.format(name_in))))
-    update_no_2 = data[:, 0]
-    y = data[:, 1]
-
-    eq = update_no == update_no_2
-    if (isinstance(eq, bool) and not eq) or (isinstance(eq, np.ndarray) and not all(eq)):
-        assert False, "Update Numbers must match for {}.\n{}\n{}".format(name_in, str(update_no), str(update_no_2))
-
-    np.savetxt(os.path.join(dirname, 'extract-{}-vs-md_time.tsv'.format(name_in)),
-               np.column_stack([md_time, y]))
-
-
-def convert_time_to_minutes(dirname):
-    file_in = os.path.join(dirname, 'extract-seconds_for_trajectory.tsv')
-    data = np.atleast_2d(np.loadtxt(file_in))
-
-    if data.shape[1] == 0:
-        return
-
-    update_no = data[:, 0]
-    seconds = data[:, 1]
-
-    np.savetxt(os.path.join(dirname, 'extract-minutes_for_trajectory.tsv'),
-               np.column_stack([update_no, seconds / 60]))
