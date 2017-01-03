4ca76851ed3b47657c939f8d2e7cb491e7e7c5d4
Martin Ueding <dev@martin-ueding.de>
2017-01-03 22:09:38 +0100

    Add a bunch of comments

--- compilation/bootstrap-jureca.sh
+++ compilation/bootstrap-jureca.sh
@@ -4,31 +4,38 @@
 # Installs US QCD Chroma on JURECA, an Intel Xeon E5-2680 v3 Haswell + NVIDIA
 # K40 supercomputer.
 
+# License (MIT/Expat)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 set -e
 set -u
 set -x
 
-# Set all locale to C, such that compiler error messages are all in English.
-# This makes debugging way easier.
-export LC_ALL=C
-
-compiler=${COMPILER-icc}
-
 if (( $# == 0 )); then
     echo "usage: $0 BASE"
 fi
-
 basedir="$PWD/$1"
 
-sourcedir="$basedir/sources"
-mkdir -p "$sourcedir"
-
-prefix="$basedir/local-$compiler"
-mkdir -p "$prefix"
-
-build="$basedir/build-$compiler"
-mkdir -p "$build"
-
+# Set all locale to C, such that compiler error messages are all in English.
+# This makes debugging way easier.
+export LC_ALL=C
 
 # Load the appropriate modules and output the present versions.
 set +x
@@ -37,7 +44,12 @@ module load Autotools
 module list
 set -x
 
-# Basic flags that are used for every single project compiled.
+# The best compiler for JURECA is the Intel C++. You can choose a different
+# compiler setting the environment `COMPILER` variable when calling this
+# script.
+compiler=${COMPILER-icc}
+
+# Set up the chosen compiler.
 case $compiler in
     icc)
         set +x
@@ -53,6 +65,8 @@ case $compiler in
         cxx11_flags="--std=c++11"
         disable_warnings_flags="-Wno-all -Wno-pedantic"
         qphix_flags="-xAVX -qopt-report -qopt-report-phase=vec -restrict"
+        # QPhiX can make use of the Intel “C Extended Array Notation”, this
+        # gets enabled here.
         qphix_configure="--enable-cean"
         ;;
     gcc)
@@ -72,11 +86,29 @@ case $compiler in
         qphix_configure=""
         ;;
     *)
-        echo "This compiler is not supported by this script. Choose another one."
+        echo 'This compiler is not supported by this script. Choose another one or add another block to the `case` in this script.'
         exit 1
         ;;
 esac
 
+# Directory where the git repositories reside.
+sourcedir="$basedir/sources"
+mkdir -p "$sourcedir"
+
+# Directory for the installed files (headers, libraries, executables). This
+# contains the chosen compiler such that multiple compilers can be used
+# simultaneously.
+prefix="$basedir/local-$compiler"
+mkdir -p "$prefix"
+
+# Directory for building. The GNU Autotools support out-of-tree builds which
+# allow to use different compilers on the same codebase.
+build="$basedir/build-$compiler"
+mkdir -p "$build"
+
+# The GNU Autotools install `X-config` programs that let a dependent library
+# query the `CFLAGS` and `CXXFLAGS` used in the compilation. This needs to be
+# in the `$PATH`, otherwise libraries cannot be found properly.
 PATH=$prefix/bin:$PATH
 
 base_cxxflags="$base_flags"
@@ -101,25 +133,9 @@ clone-if-needed() {
     fi
 }
 
-wget-if-needed() {
-    local url="$1"
-    local dir="$2"
-
-    if ! [[ -d "$dir" ]]; then
-        base=${url##*/}
-        wget $url
-        tar -xf $base
-    fi
-
-    rm -f configure Makefile
-}
-
 # Runs `make && make install` with appropriate flags that make compilation
 # parallel on multiple cores. A sentinel file is created such that `make` is
 # not invoked once it has correctly built.
-make_smp_template="-j $(nproc)"
-make_smp_flags="${SMP-$make_smp_template}"
-
 make-make-install() {
     if ! [[ -f build-succeeded ]]; then
         nice make $make_smp_flags
@@ -131,6 +147,13 @@ make-make-install() {
     fi
 }
 
+# If the user has not given a variable `SMP` in the environment, use as many
+# processes to compile as there are cores in the system.
+make_smp_template="-j $(nproc)"
+make_smp_flags="${SMP-$make_smp_template}"
+
+# Prints a large heading such that it is clear where one is in the compilation
+# process. This is not needed but occasionally helpful.
 print-fancy-heading() {
     set +x
     echo "######################################################################"
@@ -184,9 +207,7 @@ cd "$sourcedir"
 ###############################################################################
 
 repo=qmp
-
 print-fancy-heading $repo
-
 clone-if-needed https://github.com/usqcd-software/qmp.git $repo master
 
 pushd $repo
@@ -210,9 +231,7 @@ popd
 ###############################################################################
 
 repo=libxml2
-
 print-fancy-heading $repo
-
 clone-if-needed git://git.gnome.org/libxml2 $repo v2.9.4
 
 pushd $repo
@@ -260,9 +279,7 @@ popd
 ###############################################################################
 
 repo=qdpxx
-
 print-fancy-heading $repo
-
 clone-if-needed https://github.com/usqcd-software/qdpxx.git $repo devel
 
 pushd $repo
@@ -292,9 +309,7 @@ popd
 ###############################################################################
 
 repo=qphix
-
 print-fancy-heading $repo
-
 clone-if-needed https://github.com/JeffersonLab/qphix.git $repo devel
 
 pushd $repo
@@ -332,9 +347,7 @@ popd
 ###############################################################################
 
 repo=chroma
-
 print-fancy-heading $repo
-
 clone-if-needed https://github.com/JeffersonLab/chroma.git $repo devel
 
 set +x
@@ -366,3 +379,5 @@ if ! [[ -f Makefile ]]; then
 fi
 make-make-install
 popd
+
+# vim: spell
