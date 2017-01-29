cc24af086c6ddfac6a6f633b6ffe6089acd50626
Martin Ueding <dev@martin-ueding.de>
2017-01-29 11:27:15 +0100

    Add comments, remove dead code

--- analysis/physical_point/physical_point.py
+++ analysis/physical_point/physical_point.py
@@ -28,91 +28,53 @@ def main():
     options = _parse_args()
     R = 1000
 
+    # Read in the data from the paper.
     a_inv_val = 1616
     a_inv_err = 20
     a_inv_dist = bootstrap.make_dist(a_inv_val, a_inv_err, n=R)
-
     aml, ams, l, t, trajectories, ampi_val, ampi_err, amk_val, amk_err, f_k_f_pi_val, f_k_f_pi_err = util.load_columns('physical_point/gmor.txt')
     ampi_dist = bootstrap.make_dist(ampi_val, ampi_err, n=R)
     amk_dist = bootstrap.make_dist(amk_val, amk_err, n=R)
 
-    #fig, ax = util.make_figure()
-    #ax.plot(amk_val**2 - 0.5 * ampi_val**2, ampi_val**2, linestyle='none', marker='o')
-    #ax.set_xlabel(r'$(aM_K)^2 - \frac{1}{2} (a M_\pi)^2$')
-    #ax.set_ylabel(r'$(a M_\pi)^2$')
-    #util.save_figure(fig, 'plot-mpi2')
-
-    #fig, ax = util.make_figure()
-    #ax.plot(amk_val**2 - 0.5 * ampi_val**2, amk_val**2, linestyle='none', marker='o')
-    #ax.set_xlabel(r'$(aM_K)^2 - \frac{1}{2} (a M_\pi)^2$')
-    #ax.set_ylabel(r'$(a M_K)^2$')
-    #util.save_figure(fig, 'plot-mk2')
-
-    #fig, ax = util.make_figure()
-    #ax.plot(amk_val**2 - 0.5 * ampi_val**2, ams, linestyle='none', marker='o')
-    #ax.set_xlabel(r'$(aM_K)^2 - \frac{1}{2} (a M_\pi)^2$')
-    #ax.set_ylabel(r'$a m_s$')
-    #util.save_figure(fig, 'plot-ms')
-
-    #fig, ax = util.make_figure()
-    #ax.plot(amk_val**2 - 0.5 * ampi_val**2, aml, linestyle='none', marker='o')
-    #ax.set_xlabel(r'$(aM_K)^2 - \frac{1}{2} (a M_\pi)^2$')
-    #ax.set_ylabel(r'$a m_l$')
-    #util.save_figure(fig, 'plot-ml')
-
+    # Convert the data in lattice units into physical units.
     mpi_dist = [a_inv * ampi for ampi, a_inv in zip(ampi_dist, a_inv_dist)]
     mpi_val, mpi_avg, mpi_err = bootstrap.average_and_std_arrays(mpi_dist)
-
     mpi_sq_dist = [mpi**2 for mpi in mpi_dist]
     mpi_sq_val, mpi_sq_avg, mpi_sq_err = bootstrap.average_and_std_arrays(mpi_sq_dist)
-
     ampi_sq_dist = [ampi**2 for ampi in ampi_dist]
     ampi_sq_val, ampi_sq_avg, ampi_sq_err = bootstrap.average_and_std_arrays(ampi_sq_dist)
 
+    # Do a GMOR fit in order to extract `a B` and `a m_cr`.
     popt_dist = [op.curve_fit(gmor_pion, aml, ampi_sq)[0]
                  for ampi_sq in ampi_sq_dist]
     aB_dist = [popt[0] for popt in popt_dist]
     amcr_dist = [popt[1] for popt in popt_dist]
-
     aB_val, aB_avg, aB_err = bootstrap.average_and_std_arrays(aB_dist)
     amcr_val, amcr_avg, amcr_err = bootstrap.average_and_std_arrays(amcr_dist)
-
     print('aB =', siunitx(aB_val, aB_err))
     print('am_cr =', siunitx(amcr_val, amcr_err))
 
-    # Values from FLAG paper.
+    # Physical meson masses from FLAG paper.
     mpi_phys_dist = bootstrap.make_dist(134.8, 0.3, R)
     mk_phys_dist = bootstrap.make_dist(494.2, 0.3, R)
-
     ampi_phys_dist = [mpi_phys / a_inv
                       for a_inv, mpi_phys in zip(a_inv_dist, mpi_phys_dist)]
     amk_phys_dist = [mk_phys / a_inv
                      for a_inv, mk_phys in zip(a_inv_dist, mk_phys_dist)]
-
     ampi_phys_val, ampi_phys_avg, ampi_phys_err = bootstrap.average_and_std_arrays(ampi_phys_dist)
     amk_phys_val, amk_phys_avg, amk_phys_err = bootstrap.average_and_std_arrays(amk_phys_dist)
-
     print('aM_pi phys =', siunitx(ampi_phys_val, ampi_phys_err))
     print('aM_k phys =', siunitx(amk_phys_val, amk_phys_err))
 
-    print('ampi_dist.shape:', ampi_dist.shape)
-    print('amk_dist.shape:', amk_dist.shape)
-    print('aB_dist.shape:', len(aB_dist))
-    print('amcr_dist.shape:', len(amcr_dist))
-
+    # Compute the strange quark mass that is needed to obtain a physical meson
+    # mass difference, ignoring lattice artifacts.
     ams_phys_dist = [
         (amk_phys**2 - 0.5 * ampi_phys**2) / aB - amcr
         for ampi_phys, amk_phys, aB, amcr in zip(ampi_phys_dist, amk_phys_dist, aB_dist, amcr_dist)]
-
     ams_phys_cen, ams_phys_val, ams_phys_err = bootstrap.average_and_std_arrays(ams_phys_dist)
-
     print('M_K = {} MeV <== am_s ='.format(siunitx(494.2, 0.3)), siunitx(ams_phys_cen, ams_phys_err))
-
     aml_phys_dist = [op.newton(lambda aml: gmor_pion(aml, *popt) - ampi_phys**2, np.min(aml))
                      for popt, ampi_phys in zip(popt_dist, ampi_phys_dist)]
-
-    #aml_240_dist = [op.newton(lambda aml: gmor_pion(aml, *popt) - 240**2, np.min(aml))
-    #                for popt in popt_dist]
 
     fit_x = np.linspace(np.min(aml_phys_dist), np.max(aml), 100)
     fit_y_dist = [np.sqrt(gmor_pion(fit_x, *popt)) * a_inv
