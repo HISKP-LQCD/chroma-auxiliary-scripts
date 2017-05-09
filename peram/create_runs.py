002c689a2d631a334f3bc91796d622dd1173d170
Martin Ueding <dev@martin-ueding.de>
2017-05-09 16:31:18 +0200

    Finish the scripts

--- peram/create_runs.py
+++ peram/create_runs.py
@@ -17,64 +17,69 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('conf_start', type=int)
     parser.add_argument('conf_end', type=int)
-    parser.add_argument('conf_step', type=int, default=8)
+    parser.add_argument('conf_step', type=int, nargs='?', default=8)
     parser.add_argument('--email', default='')
-    parser.add_argument('--evdir', default='\/hiskp2\/eigensystems\/0120-Mpi270-L24-T96\/hyp_062_062_3\/nev_120')
-    parser.add_argument('--gconfbase', default='\/hiskp2\/gauges\/0120_Mpi270_L24_T96\/stout_smeared\/conf')
-    parser.add_argument('--exec', default='\/hadron\/bartek\/bin\/peram_gen\/peram_gen.multigpu.hybrid.quda-v0.7.2.openmpi')
+    parser.add_argument('--evdir', default='/hiskp2/eigensystems/0120-Mpi270-L24-T96/hyp_062_062_3/nev_120')
+    parser.add_argument('--gconfbase', default='/hiskp2/gauges/0120_Mpi270_L24_T96/stout_smeared/conf')
+    parser.add_argument('--exe', default='/hadron/bartek/bin/peram_gen/peram_gen.multigpu.hybrid.quda-v0.7.2.openmpi')
     parser.add_argument('--jobname', default='sWC_A2p1_Mpi270_L24T96_s')
-    parser.add_argument('--rundir', default='\/hiskp2\/ueding\/peram_generation\/sWC_A2p1_Mpi270_L24T96\/${flavour}\/cnfg')
-    parser.add_argument('--quda_rsc_path', default='\/hadron\/ueding\/quda_rsc_path\/')
-    options = parser.parse_arguments()
+    parser.add_argument('--rundir', default='/hiskp2/ueding/peram_generation/sWC_A2p1_Mpi270_L24T96/${flavour}/cnfg')
+    parser.add_argument('--quda_rsc_path', default='/hadron/ueding/quda_rsc_path/')
+    options = parser.parse_args()
 
     # Set up Jinja.
     env = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
     template_invert = env.get_template("quda.invert.input.j2")
-    template_laph = env.get_template("quda.job.pbs.sh.j2")
-    template_pbs = env.get_template("quda.LapH.in.j2")
+    template_pbs = env.get_template("quda.job.pbs.sh.j2")
+    template_laph = env.get_template("quda.LapH.in.j2")
 
-    for cfg_id in range(options.conf_start, options.conf_end + 1, options.step):
+    for cfg_id in range(options.conf_start, options.conf_end + 1, options.conf_step):
         print('Creating scripts for configuration', cfg_id)
 
-        cfg_dir = 'cnfg{:04d}'.format(cfg_id)
-        os.makedirs(os.path.join(cfg_dir, 'outputs'), exist_ok=True)
+        for flavor, kappa, quarktype in [('light', 0.129082225, 'u'),
+                                         ('strange', 0.127064803, 's')]:
+            cfg_dir = os.path.join(flavor, 'cnfg{:04d}'.format(cfg_id))
+            os.makedirs(os.path.join(cfg_dir, 'outputs'), exist_ok=True)
+            rundir = os.path.join(options.rundir, flavor, 'cnfg')
 
-        for flavor, kappa in [('light', 'strange')]:
+            for rv, seed in enumerate(seeds):
+                rv_dir = os.path.join(cfg_dir, 'rnd_vec_{:02d}'.format(rv))
+                os.makedirs(rv_dir, exist_ok=True)
 
-        for rv, seed in enumerate(seeds):
-            rv_dir = os.path.join(cfg_dir, 'rnd_vec_{:02d}'.format(rv))
-            os.makedirs(rv_dir, exist_ok=True)
+                laphin = 'LapH_{:04d}_{:02d}.in'.format(cfg_id, rv)
+                jscr = 'quda.job.pbs.{:04d}_{:02d}.sh'.format(cfg_id, rv)
+                outfile = '../outputs/run_{:04d}_{:02d}.out'.format(cfg_id, rv)
 
-            laphin = 'LapH_{:04d}_{:2d}.in'.format(cfg_id, rv)
-            jscr = 'quda.job.pbs.{:04d}_{:02d}.sh'.format(cfg_id, rv)
-            outfile = '../outputs/run_{:04d}_{:02d}.out'.format(cfg_id, rv)
+                with open(os.path.join(rv_dir, laphin), 'w') as f:
+                    f.write(template_laph.render(
+                        rv=rv,
+                        i=cfg_id,
+                        seed=seed,
+                        quarktype=quarktype,
+                        evdir=options.evdir,
+                    ))
 
-            with open(laphin, 'w') as f:
-                f.write(template_laph.render(
-                    rv=rv,
-                    seed=seed,
-                    quarktype=quarktype,
-                    evdir=evdir,
-                ))
+                with open(os.path.join(rv_dir, jscr), 'w') as f:
+                    f.write(template_pbs.render(
+                        jobname=options.jobname,
+                        i=cfg_id,
+                        email_address=options.email,
+                        rv=rv,
+                        exe=options.exe,
+                        outfile=outfile,
+                        laphin=laphin,
+                        quda_rsc_path=options.quda_rsc_path,
+                    ))
 
-            with open(jscr, 'w') as f:
-                f.write(template_pbs.render(
-                    jobname=jobname,
-                    i=cfg_id,
-                    email_address=options.email,
-                    rv=rv,
-                    exec=exec,
-                    outfile=outfile,
-                    laphin=laphin,
-                    quda_rsc_path=quda_rsc_path,
-                ))
+                with open(os.path.join(rv_dir, 'invert.input'), 'w') as f:
+                    f.write(template_invert.render(
+                        i=cfg_id,
+                        kappa=kappa,
+                        gconfbase=options.gconfbase,
+                    ))
 
-            with open(jscr, 'w') as f:
-                f.write(template_pbs.render(
-                    i=cfg_id,
-                    kappa=kappa,
-                    gconfbase=gconfbase,
-                ))
+    os.makedirs(options.quda_rsc_path, exist_ok=True)
+
 
 if __name__ == "__main__":
     main()
